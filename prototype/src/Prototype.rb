de6d775e89f654533c3a15d497c90ec7e991110c
Gabriel Damián Montenegro Aguilar <86801259+gabriel1816@users.noreply.github.com>
2024-04-27 00:42:31 -0300

    prueba gabi tadp

--- prototype/src/Prototype.rb
+++ prototype/src/Prototype.rb
@@ -1,22 +1,41 @@
 module Prototype
-
+  def initialize(prototipo = nil)
+    @prototipo = prototipo
+  end
   def set_property(attribute, value)
 
-    self.class.send(:attr_accessor, attribute)
-    instance_variable_set("@#{attribute}", value)
+    define_singleton_method(attribute) do ## uso define single method porque define method establece un método de clase
+                                          # y por lo tanto todas las instancias van a decir lo mismo
+      value
+    end
+    define_singleton_method("#{attribute}=") do |new_value|  #defino también el getter
+      value = new_value
+    end
 
   end
 
-  def set_property2(attribute, value)
-
-    self.singleton_class.send(:attr_accessor, attribute)
-    self.send("#{attribute}=", value)
-  end
-  def set_method(name, method)
-    self.class.define_method(name, &method)
+  def set_method(nombre, bloque)
+    define_singleton_method(nombre) do |*args|
+      instance_exec(*args, &bloque)              # para ejecutar el bloque sin parametros instance_eval, con parámetros instances_exec
+    end
   end
 
-  def set_method2(name, method)
-    define_singleton_method(name, &method)
+  def set_prototype(prototipo)
+
+    @prototipo = prototipo
   end
+
+  def respond_to_missing(method_name)
+    @prototipo.respond_to?(method_name) || super
+  end
+
+  def method_missing(method_name, *params, &block)
+    if @prototipo.respond_to?(method_name)
+      @prototipo.send(method_name, *params, &block)
+    else
+      super
+    end
+  end
+
+
 end
